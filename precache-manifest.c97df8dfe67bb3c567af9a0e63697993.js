c2ff50242a8dd01357be04d74a47dabe697159f7
Arvind <arvind.pandit@indrones.com>
2024-06-23 15:22:33 +0530

    Updates

---
+++ precache-manifest.c97df8dfe67bb3c567af9a0e63697993.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f506bb0bfed684e0102df48d585a3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f682a99857eae6f101e",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "f7dccbca68bfe9e5f7eb",
+    "url": "/static/js/2.1d415670.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.1d415670.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f682a99857eae6f101e",
+    "url": "/static/js/main.014c36ef.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "c1adc70b853455b30569c5396b69d56a",
+    "url": "/static/media/resume.c1adc70b.pdf"
+  }
+]);
